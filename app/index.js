531eb2a22dd4fe7591def1570610c4cc96422163
dphaener <dphaener@gmail.com>
2015-09-15 09:59:54 -0700

    Remove unneeded chalk import

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
  
 var LegitGenerator = yeoman.generators.Base.extend({
   promptUser: function() {
